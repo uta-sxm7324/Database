415f51e422830875c9cb55f12ac7d89f864b97db
uta-sxm7324 <sxm7324@mavs.uta.edu>
2025-04-17 13:38:39 -0500

    Backend refactoring

---
+++ Database.js
@@ -0,0 +1,41 @@
+class Database {
+    constructor(connection) {
+        this.connection = connection;
+        this.dbName = "arlingtonorganicmarket";
+    }
+
+    init() {
+        this.connection.query(
+            'SHOW DATABASES LIKE ?',
+            [this.dbName],
+            (err, results) => {
+              if (err) throw err;
+          
+              if (results.length > 0) {
+                console.log(`Database "${this.dbName}" exists. Using it`);
+              } else {
+                console.log(`Database "${this.dbName}" not found. Creating...`);
+          
+                // Read SQL file
+                const sql = fs.readFileSync('arlingtonorganicmarket.sql', 'utf8');
+          
+                // Run SQL file (it should include CREATE DATABASE and other commands)
+                connection.query(sql, (err, results) => {
+                  if (err) throw err;
+                  console.log(`Database "${this.dbName}" created from name.sql`);
+                });
+              }
+            }
+          );
+    }
+
+    getTable(table) {
+        this.connection.query("SELECT * FROM " + table, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+            return result;
+        });
+    }
+}
+
+module.exports = Database;
